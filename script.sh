09d7e37819fea382b490b6fe04249c19a474635b
SonicGFX <sonicgfx037@gmail.com>
2025-07-20 18:18:05 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 
 rm -rf .repo/local_manifests
 
+rm -rf prebuilts/clang/host/linux-x86
+
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 git clone https://github.com/SonicGFX/local_manifests --depth 1 -b los23 .repo/local_manifests
 
