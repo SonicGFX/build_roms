7c243f0439d3eae1263c3ee08fc2dce1680ce3d9
SonicGFX <sonicgfx037@gmail.com>
2025-07-20 18:15:12 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/The-Pixel-Project/manifest -b 15 --git-lfs
-git clone https://github.com/SonicGFX/local_manifests --depth 1 -b pixel .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
+git clone https://github.com/SonicGFX/local_manifests --depth 1 -b los23 .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch aosp_garnet-bp1a-userdebug
+lunch lineage_garnet-bp2a-userdebug
 make bacon
