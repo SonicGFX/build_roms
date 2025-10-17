0c2258f0d557825ed254eb61c57bca8bee90fec0
SonicGFX <sonicgfx037@gmail.com>
2025-10-17 23:12:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
 
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
-git clone https://github.com/SonicGFX/local_manifests --depth 1 -b cr .repo/local_manifests
+git clone https://github.com/SonicGFX/local_manifests --depth 1 -b los .repo/local_manifests
 
 /opt/crave/resync.sh
 
