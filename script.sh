a49ecd13dd4f5e9c243d0c11e4ad3e428e90c4b9
SonicGFX <sonicgfx037@gmail.com>
2025-10-27 18:33:46 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,11 +4,11 @@ rm -rf .repo/local_manifests
 
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
-git clone https://github.com/SonicGFX/local_manifests --depth 1 -b los .repo/local_manifests
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+git clone https://github.com/SonicGFX/local_manifests --depth 1 -b cr16 .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-bp1a-userdebug
+lunch lineage_garnet-bp2a-userdebug
 mka bacon
